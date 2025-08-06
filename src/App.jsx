d13c874f9126328eaf8cd6b1a0a286bdac6780fd
0BooM <149960425+0BooM@users.noreply.github.com>
2025-08-06 19:52:26 +0200

    refactor: remove unused logo imports from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GeneralInfo from './components/GeneralInfo'
 import EducationalInfo from './components/EducationalInfo'
